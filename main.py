d1546f2b974faf8ddf20055c4a8091c09c20991e
rsa-avto <dan.company2211@gmail.com>
2025-08-19 09:13:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ from urllib.parse import unquote
 
 from aiofiles.os import remove
 
-from db_utils import execute_query
+from utils.db_utils import execute_query
 from geopy.distance import geodesic
 from telebot.apihelper import ApiTelegramException
 from telebot import types
